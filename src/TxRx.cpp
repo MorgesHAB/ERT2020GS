8df8866516b12de0124242e352dcdd6cd53fa742
MorgesHAB <lionel.isoz@gmail.com>
2019-11-20 18:43:45 +0100

    test Rx & Tx modem

---
+++ src/TxRx.cpp
@@ -0,0 +1,41 @@
+/*!
+ *      ______ _____ _______      _____  _____     ___   ___ ___   ___
+ *     |  ____|  __ \__   __|    / ____|/ ____|   |__ \ / _ \__ \ / _ \
+ *     | |__  | |__) | | |      | |  __| (___        ) | | | | ) | | | |
+ *     |  __| |  _  /  | |      | | |_ |\___ \      / /| | | |/ /| | | |
+ *     | |____| | \ \  | |      | |__| |____) |    / /_| |_| / /_| |_| |
+ *     |______|_|  \_\ |_|       \_____|_____/    |____|\___/____|\___/
+ *
+ * \file GST.cpp
+ *
+ * \brief Ground Station Transceiver
+ *
+ *
+ * \author      ISOZ Lionel - EPFL EL BA3
+ * \date        04.11.2019	
+ */
+
+#include <LoRa.h>
+#include <DataHandler.h>
+
+
+int main() {
+    // Your RF modem
+    LoRa loRa;
+    // RF packet handler
+    DataHandler dataHandler;
+
+    std::cout << "\nLoRa Reception is active... waiting for RF packet..." << std::endl;
+
+    // Automatic detection of the packet type - then auto parse
+    while (true) {
+        if (loRa.receive(dataHandler)) {
+            dataHandler.printLastRxPacket();
+            dataHandler.update(PROPULSION);
+            loRa.send(dataHandler.getPacket(PROPULSION));
+            usleep(1000000); // 1 second
+        }
+        usleep(10); //microseconds
+    }
+    return 0;
+}
---
+++ src/TxRx2.cpp
@@ -0,0 +1,43 @@
+/*!
+ *      ______ _____ _______      _____  _____     ___   ___ ___   ___
+ *     |  ____|  __ \__   __|    / ____|/ ____|   |__ \ / _ \__ \ / _ \
+ *     | |__  | |__) | | |      | |  __| (___        ) | | | | ) | | | |
+ *     |  __| |  _  /  | |      | | |_ |\___ \      / /| | | |/ /| | | |
+ *     | |____| | \ \  | |      | |__| |____) |    / /_| |_| / /_| |_| |
+ *     |______|_|  \_\ |_|       \_____|_____/    |____|\___/____|\___/
+ *
+ * \file GST.cpp
+ *
+ * \brief Ground Station Transceiver
+ *
+ *
+ * \author      ISOZ Lionel - EPFL EL BA3
+ * \date        04.11.2019	
+ */
+
+#include <LoRa.h>
+#include <DataHandler.h>
+
+
+int main() {
+    // Your RF modem
+    LoRa loRa;
+    // RF packet handler
+    DataHandler dataHandler;
+
+    std::cout << "\nLoRa Reception is active... waiting for RF packet..." << std::endl;
+
+    dataHandler.update(PAYLOAD);
+    loRa.send(dataHandler.getPacket(PAYLOAD));
+    // Automatic detection of the packet type - then auto parse
+    while (true) {
+        if (loRa.receive(dataHandler)) {
+            dataHandler.printLastRxPacket();
+            dataHandler.update(PAYLOAD);
+            loRa.send(dataHandler.getPacket(PAYLOAD));
+            usleep(1000000); // 1 second
+        }
+        usleep(10); //microseconds
+    }
+    return 0;
+}
