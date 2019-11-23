7efbc648744bb47fd29ff39984c5feeae45d1194
MorgesHAB <lionel.isoz@gmail.com>
2019-11-23 14:31:30 +0100

    change size picture

--- src/GSE.cpp
+++ src/GSE.cpp
@@ -41,7 +41,6 @@ int main(int argc, char** argv) {
     while (true) {
       //  dataHandler.update(PAYLOAD);
        // loRa.send(dataHandler.getPacket(PAYLOAD));
-
         dataHandler.update(IMAGE);
         loRa.send(dataHandler.getPacket(IMAGE));
         usleep(500000);
