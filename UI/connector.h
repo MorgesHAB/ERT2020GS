fdf4b3240c4d1ebf4b57622ab947896f44aa84b0
cem-keske <48871013+cem-keske@users.noreply.github.com>
2019-12-01 23:24:02 +0100

    Update Connector.h
    new getters and setters added

--- UI/connector.h
+++ UI/connector.h
@@ -4,17 +4,28 @@
 #include <array>
 
 
+
 class Connector
 {
 public:
-    enum DataType { ALTITUDE, count };
-
+    enum UintData { ALTITUDE = 0, count };
+    enum BoolData { SOLENOID_CLICKED = 0, count },
+    enum DoubleData { PRESSURE =0, count }
     Connector();
-    void set_data(DataType type, uint64_t data);
-    uint64_t get_data(DataType type);
+    
+    void set_data(UintData type, uint64_t data);
+    uint64_t get_data(UintData type);
+
+    void set_data(BoolData type, bool data);
+    bool get_data(BoolData type);
+    
+    void set_data(DoubleData type, double data);
+    double get_data(DoubleData type);
 
 private:
-    std::array<std::atomic_uint64_t, DataType::count> data_;
+    std::array<std::atomic_uint64_t, UintData::count> uints_;
+    std::array<std::atomic_bool, BoolData::count> bools_;
+    std::array<std::atomic_double, DoubleData> doubles_;
 };
 
 
