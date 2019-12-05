a070ad25dde495f0042b85e772ddfe06197409ba
Cem Keske <cem.keske@epfl.ch>
2019-12-05 12:58:19 +0100

    pusher committer added

---
+++ UI/committer.sh
@@ -0,0 +1,2 @@
+git commit gpswindow.cpp gpswindow.h ui_form.h gui_test.cc QTProject.pro pusher.sh committer.sh
+
---
+++ UI/pusher.sh
@@ -0,0 +1,6 @@
+./committer.sh
+git push 
+git checkout master
+git merge gui_branch
+git push
+git checkout gui_branch
