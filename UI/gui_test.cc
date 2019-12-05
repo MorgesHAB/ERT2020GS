f30629026c3381bd7e8c8e6ea9767a473d6d583f
Cem Keske <cem.keske@epfl.ch>
2019-12-05 12:42:00 +0100

    New gui components formed

---
+++ UI/gui_test.cc
@@ -0,0 +1,40 @@
+#include <QApplication>
+#include <memory>
+#include <thread>
+#include <iostream>
+#include <chrono>
+#include "../RF-UI-Interface/connector.h"
+#include "../RF-UI-Interface/ProtocolDefine.h"
+#include "gpswindow.h"
+
+class A{
+public:
+void f1(std::shared_ptr<Connector> c){
+    unsigned a(0);
+    while(c->getData<bool>(ui_interface::RUNNING) ){
+        c->setData(ui_interface::ALTITUDE,a);
+        std::cout << "Data set to : " << a << std::endl;
+        a++;
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    }
+}
+};
+
+int main(int argc, char **argv) {
+    //set up everything
+    Connector c;
+    A a;
+    QApplication app(argc, argv);
+
+    GPSWindow w(500, std::shared_ptr<Connector>(&c));
+    //run all threads
+    std::thread receiver_thread(&A::f1, a, std::shared_ptr<Connector>(&c));
+
+
+
+    //end the program
+    w.show();
+    app.exec();
+    receiver_thread.join();
+    return 0;
+}
