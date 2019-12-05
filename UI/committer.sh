ec31676127fbe91b887474c07449d55c880a9fc4
Cem Keske <cem.keske@epfl.ch>
2019-12-05 16:24:27 +0100

    ui new

--- UI/committer.sh
+++ UI/committer.sh
@@ -1,2 +1,2 @@
-git commit gpswindow.cpp gpswindow.h ui_form.h gui_test.cc QTProject.pro pusher.sh committer.sh ../RF-UI-Interface/connector.cpp ../RF-UI-Interface/connector.h ../RF-UI-Interface/ProtocolDefine.h
+git commit GuiWindow.cpp GuiWindow.h ui_form.h gui_test.cc QTProject.pro pusher.sh committer.sh ../RF-UI-Interface/connector.cpp ../RF-UI-Interface/connector.h ../RF-UI-Interface/ProtocolDefine.h
 
