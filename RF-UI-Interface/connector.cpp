0a9eeea288d9373fc6ebdbe36c7d3588450ae7cd
Cem Keske <cem.keske@epfl.ch>
2019-12-05 14:37:07 +0100

    button added

--- RF-UI-Interface/connector.cpp
+++ RF-UI-Interface/connector.cpp
@@ -1,1 +1,11 @@
 #include "connector.h"
+
+constexpr bool OFF_STATE(false);
+constexpr bool ON_STATE(true);
+
+
+
+Connector::Connector(){
+    setData(ui_interface::ACTIVE_XBEE, OFF_STATE);
+    setData(ui_interface::RUNNING, ON_STATE);
+}
