3ff90d8749e20560777475127b1ec931371acbfd
Cem Keske <cem.keske@epfl.ch>
2019-12-08 15:53:20 +0100

    "now xbee can be turned on and off again

--- UI/committer.sh
+++ UI/committer.sh
@@ -1,2 +1,3 @@
 git commit GuiWindow.cpp GuiWindow.h ui_form.h gui_test.cc QTProject.pro pusher.sh committer.sh ../RF-UI-Interface/connector.cpp ../RF-UI-Interface/connector.h ../RF-UI-Interface/ProtocolDefine.h
+git commit -a
 
