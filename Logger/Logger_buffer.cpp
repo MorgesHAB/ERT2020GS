600f0adf01de4280532368db72b83f0ffdc710d8
Cem Keske <cem.keske@epfl.ch>
2020-02-17 16:22:24 +0100

    logger still not fully tested

---
+++ Logger/Logger_buffer.cpp
@@ -0,0 +1,55 @@
+#include "Logger_buffer.h"
+#include <iostream>
+
+
+constexpr size_t DEFAULT_SIZE(1000);
+
+Logger_buffer::Logger_buffer(size_t size) : size(size), ready_(true), position_(0), buffer_(size){}
+
+Logger_buffer::Logger_buffer() : size(DEFAULT_SIZE), ready_(true), position_(0), buffer_(DEFAULT_SIZE){ }
+
+size_t Logger_buffer::position()
+{
+    return position_;
+}
+
+void Logger_buffer::add(const std::string &value)
+{
+    if(not ready_)
+        std::cerr << "Trying to write into a buffer which is not ready, data will be overridden" << std::endl;
+    buffer_[position_] = value;
+    ++position_;
+    if (position_ >= size) {
+        ready_ = false;
+    }
+}
+
+void Logger_buffer::make_ready_to_fill()
+{
+    ready_ = true;
+    position_ = 0;
+}
+
+bool Logger_buffer::is_ready_to_fill() const
+{
+    return ready_;
+}
+
+bool Logger_buffer::is_full() const
+{
+    return !ready_;
+}
+
+std::string &Logger_buffer::operator[](size_t index)
+{
+    if(index >= size)
+        std::cerr << "INDEX OUT OF BOUNDS" << std::endl;
+    return buffer_[index];
+}
+
+void Logger_buffer::display() const
+{
+    for(auto & el : buffer_)
+        std::cout << el << std::endl;
+}
+
---
+++ Logger/Logger_buffer.h
@@ -0,0 +1,75 @@
+#ifndef LOG_BUFFER_H
+#define LOG_BUFFER_H
+
+#include <string>
+#include <vector>
+#include <atomic>
+#include <ostream>
+
+/**
+ * @brief The Logger_buffer class Used by the logger to simplify buffer utilization.
+ */
+class Logger_buffer
+{
+public:
+    /**
+     * @brief Logger_buffer
+     * @param size The size of the buffer.
+     */
+    Logger_buffer(size_t size);
+    /**
+     * @brief Logger_buffer Initalize a default_sized buffer.
+     */
+    Logger_buffer();
+
+    /**
+     * @brief position The current position on the buffer.
+     * @return The current position on the buffer.
+     */
+    size_t position();
+
+    /**
+     * @brief add Adds a new string to the buffer.
+     * @param value The string to add.
+     */
+    void add(const std::string& value);
+
+    /**
+     * @brief make_ready_to_fill Sets the buffer as ready to fill.
+     */
+    void make_ready_to_fill();
+    /**
+     * @brief is_ready_to_fill
+     * @return If the buffer is ready to fill. (if it still has some space)
+     */
+    bool is_ready_to_fill() const;
+    /**
+     * @brief is_full
+     * @return If the buffer is not ready to fill.
+     */
+    bool is_full() const;
+    /**
+     * @brief operator []
+     * @return Accessor to the elements of the buffer.
+     */
+    std::string & operator[](size_t);
+
+    /**
+     * @brief display Prints all the elements line by line.
+     */
+    void display() const;
+
+    /**
+     * @brief size The size of the buffer.
+     */
+    const size_t size;
+
+
+private:
+    std::atomic<bool> ready_; //for safety
+    std::atomic<size_t> position_;
+    std::vector<std::string> buffer_;
+
+};
+
+#endif
