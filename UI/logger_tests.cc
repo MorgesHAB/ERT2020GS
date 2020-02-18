c090925f85d742611d5bf43de1114e565cb8b907
Cem Keske <cem.keske@epfl.ch>
2020-02-18 10:06:03 +0100

    logger tests added

---
+++ UI/logger_tests.cc
@@ -0,0 +1,74 @@
+
+#include "../Logger/Logger_buffer.h"
+#include "../Logger/double_buffer.h"
+#include "../Logger/Logger.h"
+#include "../Logger/Loggable.h"
+#include <string>
+#include <chrono>
+
+
+class Data : public Loggable {
+public:
+    virtual std::string log_description() const{
+        return std::move(text);
+    }
+
+    Data(std::string str):text(str){}
+    std::string text;
+};
+
+
+
+
+void foo(size_t until){
+/*
+    Double_buffer buf;
+
+    for(size_t i=0;i<until; i++){
+        buf.add(std::to_string(i) + d.log_description());
+        std::this_thread::sleep_for(std::chrono::microseconds(2));
+        //b1.display();
+        //std::cout << i << std::endl;
+
+    }
+*/
+
+    static std::atomic<size_t> a(0);
+
+    Logger lo(std::to_string(++a));
+    for(size_t i(0); i < until; ++i){
+        lo.log(Data(std::to_string(i)));
+        std::this_thread::sleep_for(std::chrono::microseconds(2));
+    }
+}
+
+int
+    main(int argc, char ** argv)
+{
+
+
+    /*
+    for(int i=0;i<200000;i++){
+        b1.add("sentence" + std::to_string(i));
+        std::this_thread::sleep_for(std::chrono::microseconds(2));
+        //b1.display();
+        std::cout << i << std::endl;
+        if(i==47868)
+            return 0;
+    }
+*/
+
+    std::thread t1(foo, 100);
+    std::thread t2(foo, 1111);
+    std::thread t3(foo, 2991);
+    std::thread t4(foo, 41251);
+
+    std::this_thread::sleep_for(std::chrono::seconds(3));
+
+    t1.join();
+    t2.join();
+    t3.join();
+    t4.join();
+    return 0;
+
+}
