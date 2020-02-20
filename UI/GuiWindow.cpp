d87dd2c29465648d9ea0ad1085ec0d730f64de5c
Cem Keske <cem.keske@epfl.ch>
2020-02-20 17:23:21 +0100

    uint datagramid

--- UI/GuiWindow.cpp
+++ UI/GuiWindow.cpp
@@ -273,7 +273,7 @@ void GuiWindow::refresh_com()
     refresh_misses();
     last_packet_number_panel->setText(
             qstr(data_->getData<uint32_t>(TX_PACKET_NR)));
-    std::string str(DatagramType::getDatagramIDName(data_->getData<DatagramType::DatagramID>(ui_interface::DATAGRAM_ID)));
+    std::string str(DatagramType::getDatagramIDName(data_->getData<uint8_t>(ui_interface::DATAGRAM_ID)));
     last_datagram_id_panel->setText(QString::fromStdString(str));
     received_pack_cnt_panel->setText(qstr(data_->getData<uint32_t>(RX_PACKET_CTR)));
     // this->speed_lcd->display(data_->getData<float>(SPEED)); no speed
