9e47cdff7306fa2704c40f774419a8a4caadcc36
Cem Keske <cem.keske@epfl.ch>
2020-02-20 18:53:40 +0100

    logger count

--- Logger/Logger.cpp
+++ Logger/Logger.cpp
@@ -3,10 +3,17 @@
 
 static std::atomic<size_t> logger_ctr(0);
 
-Logger::Logger(std::string filename) : buffer(filename + utilities::time_short() + "-ID" + std::to_string(++logger_ctr) +  ".log") {}
+Logger::Logger(std::string filename) : buffer(filename + utilities::time_short() + "-ID" + std::to_string(++logger_ctr) +  ".log") {
+
+}
 
 void Logger::log(Loggable* data)
 {
     buffer.add(data->log_description());
+}
+
+size_t Logger::total_logger_count()
+{
+    return logger_ctr;
 }
 
