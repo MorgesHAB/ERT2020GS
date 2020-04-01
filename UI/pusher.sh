aca8b572778ae496baa95be4a95da3e31e90e02d
Cem Keske <cem.keske@epfl.ch>
2020-04-01 17:06:34 +0200

    second gui2.0  branch added

--- UI/pusher.sh
+++ UI/pusher.sh
@@ -1,14 +1,6 @@
 ./committer.sh
 git push 
 git checkout GUI2.0
-<<<<<<< HEAD
-<<<<<<< HEAD
 git merge gui-branch2.0
-=======
-git merge gui_branch
->>>>>>> gui_branch
-=======
-git merge gui-branch2.0
->>>>>>> gui-branch2.0
 git push
 git checkout gui-branch2.0
